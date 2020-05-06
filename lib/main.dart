54cc0e3c604f988b96e148e98bdedc9113cb77e6
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-05-06 20:16:40 +0300

    - first plugin implementation in devKit project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tangemsdk/tangemsdk.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,14 +48,18 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//    setState(() {
+//      // This call to setState tells the Flutter framework that something has
+//      // changed in this State, which causes it to rerun the build method below
+//      // so that the display can reflect the updated values. If we changed
+//      // _counter without calling setState(), then the build method would not be
+//      // called again, and so nothing would appear to happen.
+//      _counter++;
+//    });
+    final callback = Callback();
+    callback.onError = () {};
+    callback.onSuccess = () {};
+    TangemSdk.scanCard(callback);
   }
 
   @override
