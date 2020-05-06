5b0e41959b9611200dbc7823a82f8fc4dd6f46a0
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-05-06 19:24:31 +0300

    - added plugin with example

---
+++ plugin/tangem_sdk_plugin/example/lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:sdk/sdk.dart';
+import 'dart:developer' as dev;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await Sdk.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter: TangemSDK plugin'),
+        ),
+        body: OutlineButton(
+          child: Text('Scan card'),
+          onPressed: () {
+            final callback = Callback();
+            callback.onSuccess = () {
+              dev.log("onSuccess");
+            };
+            callback.onError = () {
+              dev.log("onError");
+            };
+            Sdk.scanCard(callback);
+          },
+        ),
+      ),
+    );
+  }
+}
