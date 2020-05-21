2d2a35fdf6149e1c76a1038603ded1cc7b301893
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-05-21 16:26:43 +0300

    AND-402 - added response screen - fixed back navigation

---
+++ lib/app/ui/screen/response/response_main.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:devkit/app/resources/localization.dart';
+import 'package:devkit/app/ui/menu/menu.dart';
+import 'package:devkit/app/ui/widgets/basic/text_widget.dart';
+import 'package:devkit/navigation/routes.dart';
+import 'package:flutter/material.dart';
+
+class ResponseScreen extends StatelessWidget {
+  final Object arguments;
+
+  const ResponseScreen({Key key, this.arguments}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ResponseFrame(arguments: arguments);
+
+  static navigate(context, arguments) {
+    Navigator.of(context).pushNamed(Routes.RESPONSE, arguments: arguments);
+  }
+}
+
+class ResponseFrame extends StatelessWidget {
+  final Object arguments;
+
+  const ResponseFrame({Key key, this.arguments}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async{
+        Navigator.popUntil(context, (Route route) => route.isFirst);
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(Transl.of(context).screen_response_scan),
+          actions: [Menu.popupDescription()],
+        ),
+        body: ReadResponseBody(readResponse: arguments),
+      ),
+    );
+  }
+}
+
+class ReadResponseBody extends StatelessWidget {
+  final Object readResponse;
+
+  const ReadResponseBody({Key key, this.readResponse}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: TextWidget(jsonEncode(readResponse)));
+  }
+}
--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -1,21 +1,25 @@
 import 'package:devkit/app/ui/screen/card_action/scan_screen.dart';
 import 'package:devkit/app/ui/screen/card_action/sign_screen.dart';
 import 'package:devkit/app/ui/screen/main_screen.dart';
+import 'package:devkit/app/ui/screen/response/response_main.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static const MAIN = "/";
   static const SCAN = "/scan";
   static const SIGN = "/sign";
+  static const RESPONSE = "/response";
 
   static Route<dynamic> generateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case MAIN:
-        return PageRouteBuilder(pageBuilder: (_, _1, _2) => MainScreen());
+        return MaterialPageRoute(builder: (_) => MainScreen());
       case SCAN:
-        return PageRouteBuilder(pageBuilder: (_, _1, _2) => ScanScreen());
+        return MaterialPageRoute(builder: (_) => ScanScreen());
       case SIGN:
-        return PageRouteBuilder(pageBuilder: (_, _1, _2) => SignScreen());
+        return MaterialPageRoute(builder: (_) => SignScreen());
+      case RESPONSE:
+        return PageRouteBuilder(pageBuilder: (_, _1, _2) => ResponseScreen(arguments: settings.arguments));
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
