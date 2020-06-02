10bf3ff49163047d68b46a24dfac3f9b80559d23
reginalatypova <rlatypova@tangem.com>
2020-06-02 20:00:50 +0300

    AND-462 fix

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -9,15 +9,29 @@ void main() {
     // be the same as the Strings we used for the Keys in step 1.
 
     final scanItem = find.byValueKey("scan");
+    final signItem = find.byValueKey("sign");
     final floatingActionButton = find.byType("FloatingActionButton");
     final responseTextWidget = find.byValueKey('responseJson');
     final expectedResult = "OPA!";
+    //final cidButton =find.text('Read Card');
+    final cidButton =find.byValueKey('cid.btn');
 
     FlutterDriver driver;
 
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+    });
+
+    test("Test ReadCard", () async {
+      print("Checking whether an element ReadCard is present");
+      await isExist(signItem, driver);
+      print("Tap ReadCard in menu");
+      await driver.tap(signItem);
+      print("Checking whether an element ActionButton is present");
+      await isExist(cidButton, driver);
+      print("Tab action button");
+      await driver.tap(cidButton);
     });
 
     test("Test ReadCard", () async {
