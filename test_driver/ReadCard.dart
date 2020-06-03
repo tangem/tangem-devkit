ac1c88df242adde8d7530bf1ca22797fc2076458
reginalatypova <rlatypova@tangem.com>
2020-06-03 15:04:47 +0300

    AND-462 edit test

---
+++ test_driver/ReadCard.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+import 'dart:convert';
+import 'Methods.dart';
+
+class ReadCard {
+  final readItem = find.byValueKey("scan");
+  final floatingActionButton = find.byType("FloatingActionButton");
+  final responseTextWidget = find.byValueKey('responseJson');
+  final methods = Methods();
+
+
+  readCard2(driver) async {
+    print("Search for the Read Card element in the menu");
+    await methods.isExist(readItem, driver);
+    print("Click Read Card element in menu");
+    await driver.tap(readItem);
+    print("Search action button element");
+    await methods.isExist(floatingActionButton, driver);
+    print("Click Read Card element in menu");
+    await driver.tap(floatingActionButton);
+    //ToDo: Robot. A code to rotate the robot.
+    String getResponse = await methods.getResponce(responseTextWidget, driver);
+    Map<String, dynamic> responce = jsonDecode(getResponse);
+    //print('${responce['cardId']}');
+    return responce;
+  }
+
+}
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 import 'dart:convert';
+import 'ReadCard.dart';
 
 void main() {
   group('Tangem DevKit app', () {
@@ -15,6 +16,7 @@ void main() {
     final expectedResult = "OPA!";
     //final cidButton =find.text('Read Card');
     final cidButton =find.byValueKey('cid.btn');
+    final readCardMethot = ReadCard();
 
     FlutterDriver driver;
 
@@ -23,48 +25,56 @@ void main() {
       driver = await FlutterDriver.connect();
     });
 
-    test("Test ReadCard", () async {
-      print("Checking whether an element ReadCard is present");
-      await isExist(signItem, driver);
-      print("Tap ReadCard in menu");
-      await driver.tap(signItem);
-      print("Checking whether an element ActionButton is present");
-      await isExist(cidButton, driver);
-      print("Tab action button");
-      await driver.tap(cidButton);
-    });
-
-    test("Test ReadCard", () async {
-      print("Start Read Card test");
-
-      print("Checking whether an element ReadCard is present");
-      await isExist(scanItem, driver);
-      print("Tap ReadCard in menu");
-      await driver.tap(scanItem);
-
-      print("Checking whether an element ActionButton is present");
-      await isExist(floatingActionButton, driver);
-      print("Tab action button");
-      await driver.tap(floatingActionButton);
-
-      print("Checking whether an element ResponseText is present");
-      await isExist(responseTextWidget, driver);
-
-      //print("Reconciliation of Results and Expected Result");
-      //print(driver.getText(responseText));
-     // expect(await driver.getText(responseText), expectedResult);
-
-      String response = await getResponce(driver,responseTextWidget);
-      Map<String, dynamic> responceJson = jsonDecode(response);
-      //print('${responceJson['cardId']}');
-      var cardId = responceJson['cardId'];
-
+    test("TEST",() {
+      print("START");
+      final responce = readCardMethot.readCard2(driver);
       var expectCardId = "bb03000000000004";
+      expect(responce['cardId'], expectCardId);
 
-      print("Reconciliation of Results and Expected Result");
-      expect(cardId, expectCardId);
-      
     });
+
+//    test("Test ReadCard", () async {
+//      print("Checking whether an element ReadCard is present");
+//      await isExist(signItem, driver);
+//      print("Tap ReadCard in menu");
+//      await driver.tap(signItem);
+//      print("Checking whether an element ActionButton is present");
+//      await isExist(cidButton, driver);
+//      print("Tab action button");
+//      await driver.tap(cidButton);
+//    });
+
+//    test("Test ReadCard", () async {
+//      print("Start Read Card test");
+//
+//      print("Checking whether an element ReadCard is present");
+//      await isExist(scanItem, driver);
+//      print("Tap ReadCard in menu");
+//      await driver.tap(scanItem);
+//
+//      print("Checking whether an element ActionButton is present");
+//      await isExist(floatingActionButton, driver);
+//      print("Tab action button");
+//      await driver.tap(floatingActionButton);
+//
+//      print("Checking whether an element ResponseText is present");
+//      await isExist(responseTextWidget, driver);
+//
+//      //print("Reconciliation of Results and Expected Result");
+//      //print(driver.getText(responseText));
+//     // expect(await driver.getText(responseText), expectedResult);
+//
+//      String response = await getResponce(driver,responseTextWidget);
+//      Map<String, dynamic> responceJson = jsonDecode(response);
+//      //print('${responceJson['cardId']}');
+//      var cardId = responceJson['cardId'];
+//
+//      var expectCardId = "bb03000000000004";
+//
+//      print("Reconciliation of Results and Expected Result");
+//      expect(cardId, expectCardId);
+//
+//    });
 
     tearDownAll(() async {
       await Future.delayed(Duration(seconds: 3));
