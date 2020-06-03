a16030b51c8e1e0dd657c3762d73d71f0f94665f
reginalatypova <rlatypova@tangem.com>
2020-06-03 16:00:59 +0300

    AND-462 sign

--- test_driver/SignCard.dart
+++ test_driver/SignCard.dart
@@ -1,19 +1,32 @@
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
 import 'dart:convert';
 import 'Methods.dart';
 
-class ReadCard {
-  final readItem = find.byValueKey("scan");
+class SignCard {
+  final signItem = find.byValueKey("sign");
+  final readCardButton =find.byValueKey('cid.btn');
+  final cidTextEdit =find.byValueKey('cid');
+  final dataForHashingTextEdit =find.byValueKey('dataForHashing');
   final floatingActionButton = find.byType("FloatingActionButton");
   final responseTextWidget = find.byValueKey('responseJson');
   final methods = Methods();
 
 
-  readCard2(driver) async {
-    print("Search for the Read Card element in the menu");
-    await methods.isExist(readItem, driver);
-    print("Click Read Card element in menu");
-    await driver.tap(readItem);
+  signCard(driver) async {
+    print("Search for the Sign element in the menu");
+    await methods.isExist(signItem, driver);
+    print("Click Sign element in menu");
+    await driver.tap(signItem);
+    if (cid==null) {
+      await methods.isExist(readCardButton, driver);
+      await driver.tap(readCardButton);
+      //ToDo: Robot. A code to rotate the robot.
+    }
+    else {
+
+
+    }
     print("Search action button element");
     await methods.isExist(floatingActionButton, driver);
     print("Click action button");
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -4,31 +4,30 @@ import 'dart:convert';
 import 'ReadCard.dart';
 
 void main() {
+
+  final scanItem = find.byValueKey("scan");
+  final signItem = find.byValueKey("sign");
+  final floatingActionButton = find.byType("FloatingActionButton");
+  final responseTextWidget = find.byValueKey('responseJson');
+  final expectedResult = "OPA!";
+  //final cidButton =find.text('Read Card');
+  final cidButton =find.byValueKey('cid.btn');
+  final readCardMethot = ReadCard();
+
+  FlutterDriver driver;
+
+  // Connect to the Flutter driver before running any tests.
+  setUpAll(() async {
+    driver = await FlutterDriver.connect();
+  });
+
   group('Tangem DevKit app', () {
-    // First, define the Finders and use them to locate widgets from the
-    // test suite. Note: the Strings provided to the `byValueKey` method must
-    // be the same as the Strings we used for the Keys in step 1.
 
-    final scanItem = find.byValueKey("scan");
-    final signItem = find.byValueKey("sign");
-    final floatingActionButton = find.byType("FloatingActionButton");
-    final responseTextWidget = find.byValueKey('responseJson');
-    final expectedResult = "OPA!";
-    //final cidButton =find.text('Read Card');
-    final cidButton =find.byValueKey('cid.btn');
-    final readCardMethot = ReadCard();
-
-    FlutterDriver driver;
-
-    // Connect to the Flutter driver before running any tests.
-    setUpAll(() async {
-      driver = await FlutterDriver.connect();
-    });
-
-    test("TEST",() {
+    test("TEST",() async {
       print("START");
-      final responce = readCardMethot.readCard2(driver);
+      final responce = await readCardMethot.readCard2(driver);
       var expectCardId = "bb03000000000004";
+      print("Reconciliation of Results and Expected Result");
       expect(responce['cardId'], expectCardId);
 
     });
@@ -44,64 +43,9 @@ void main() {
 //      await driver.tap(cidButton);
 //    });
 
-//    test("Test ReadCard", () async {
-//      print("Start Read Card test");
-//
-//      print("Checking whether an element ReadCard is present");
-//      await isExist(scanItem, driver);
-//      print("Tap ReadCard in menu");
-//      await driver.tap(scanItem);
-//
-//      print("Checking whether an element ActionButton is present");
-//      await isExist(floatingActionButton, driver);
-//      print("Tab action button");
-//      await driver.tap(floatingActionButton);
-//
-//      print("Checking whether an element ResponseText is present");
-//      await isExist(responseTextWidget, driver);
-//
-//      //print("Reconciliation of Results and Expected Result");
-//      //print(driver.getText(responseText));
-//     // expect(await driver.getText(responseText), expectedResult);
-//
-//      String response = await getResponce(driver,responseTextWidget);
-//      Map<String, dynamic> responceJson = jsonDecode(response);
-//      //print('${responceJson['cardId']}');
-//      var cardId = responceJson['cardId'];
-//
-//      var expectCardId = "bb03000000000004";
-//
-//      print("Reconciliation of Results and Expected Result");
-//      expect(cardId, expectCardId);
-//
-//    });
-
     tearDownAll(() async {
       await Future.delayed(Duration(seconds: 3));
       driver?.close();
     });
   });
-}
-
-isExist(parameter, driver) async{
-  final isExists = await isPresent(parameter, driver);
-  if (isExists) {
-    print('Widget is present');
-  } else {
-    print('Widget is not present');
-  }
-}
-
-isPresent(SerializableFinder byValueKey, FlutterDriver driver, {Duration timeout = const Duration(seconds: 3)}) async {
-  try {
-    await driver.waitFor(byValueKey,timeout: timeout);
-    return true;
-  } catch(exception) {
-    return false;
-  }
-}
-
-Future<String> getResponce(driver, responceElement) async {
-  final response = await driver.getText(responceElement);
-  return response;
 }
