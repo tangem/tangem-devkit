b913a94e6b6131cbaad24f6800a584fd8ee55d9a
reginalatypova <rlatypova@tangem.com>
2020-06-03 17:53:52 +0300

    AND-462 create sign test

--- test_driver/SignCard.dart
+++ test_driver/SignCard.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 import 'dart:convert';
@@ -13,18 +15,37 @@ class SignCard {
   final methods = Methods();
 
 
-  signCard(driver) async {
+  signCard(driver, cid, dataForHashing) async {
     print("Search for the Sign element in the menu");
     await methods.isExist(signItem, driver);
     print("Click Sign element in menu");
     await driver.tap(signItem);
-    if (cid==null) {
-      await methods.isExist(readCardButton, driver);
-      await driver.tap(readCardButton);
-      //ToDo: Robot. A code to rotate the robot.
-    }
-    else {
 
+    if (cid ==null) {
+      print("Search Read Card button");
+      await methods.isExist(readCardButton, driver);
+      print("Click Read card button");
+      await driver.tap(readCardButton);
+      sleep(const Duration(seconds: 7));
+      
+      //ToDo: Robot. A code to rotate the robot.
+      
+    } else {
+      print("Search CID input field");
+      await methods.isExist(cidTextEdit, driver);
+      await driver.tap(cidTextEdit);
+      await driver.enterText(cid);
+      final cidInput = find.text(cid);
+      await methods.isExist(cidInput, driver);
+    }
+
+    if (dataForHashing !=null) {
+      print("Search Data dof hashing input field");
+      await methods.isExist(dataForHashingTextEdit, driver);
+      await driver.tap(dataForHashingTextEdit);
+      await driver.enterText(dataForHashing);
+      final dataForHashingInput = find.text(dataForHashing);
+      await methods.isExist(dataForHashingInput, driver);
 
     }
     print("Search action button element");
@@ -32,6 +53,8 @@ class SignCard {
     print("Click action button");
     await driver.tap(floatingActionButton);
     //ToDo: Robot. A code to rotate the robot.
+    print("Wait responce");
+    sleep(const Duration(seconds: 7));
     String getResponse = await methods.getResponce(responseTextWidget, driver);
     Map<String, dynamic> responce = jsonDecode(getResponse);
     //print('${responce['cardId']}');
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -2,17 +2,13 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 import 'dart:convert';
 import 'ReadCard.dart';
+import 'SignCard.dart';
 
 void main() {
 
-  final scanItem = find.byValueKey("scan");
-  final signItem = find.byValueKey("sign");
-  final floatingActionButton = find.byType("FloatingActionButton");
-  final responseTextWidget = find.byValueKey('responseJson');
-  final expectedResult = "OPA!";
-  //final cidButton =find.text('Read Card');
-  final cidButton =find.byValueKey('cid.btn');
-  final readCardMethot = ReadCard();
+  final readCardMethod = ReadCard();
+  final signCardMethod = SignCard();
+  final backButton = find.byTooltip('Back');
 
   FlutterDriver driver;
 
@@ -21,31 +17,43 @@ void main() {
     driver = await FlutterDriver.connect();
   });
 
+  test("Test Sign Card ",() async {
+    final responceRead = await readCardMethod.readCard2(driver);
+    final cid = responceRead['cardId'];
+    final walletRemainingSignatures= responceRead['walletRemainingSignatures']-1;
+    final walletSignedHashes = responceRead['walletSignedHashes']+1;
+
+    await driver.tap(backButton);
+
+    final responceSign = await signCardMethod.signCard(driver, 'bb03000000000004', '123qwertyq');
+    final cidSign = responceSign['cardId'];
+    final walletRemainingSignaturesSign = responceSign['walletRemainingSignatures'];
+    final walletSignedHashesSign = responceSign['walletSignedHashes'];
+
+    print("Reconciliation of Results and Expected Result");
+    expect(cid, cidSign);
+    expect(walletRemainingSignatures, walletRemainingSignaturesSign);
+    //print('${responceRead['walletSignedHashes']}' + '$walletSignedHashes' +'$walletSignedHashesSign');
+    expect(walletSignedHashes, walletSignedHashesSign);
+
+  });
+
   group('Tangem DevKit app', () {
 
-    test("TEST",() async {
-      print("START");
-      final responce = await readCardMethot.readCard2(driver);
+    test("Test Read Card ",() async {
+      final responce = await readCardMethod.readCard2(driver);
       var expectCardId = "bb03000000000004";
       print("Reconciliation of Results and Expected Result");
       expect(responce['cardId'], expectCardId);
-
     });
 
-//    test("Test ReadCard", () async {
-//      print("Checking whether an element ReadCard is present");
-//      await isExist(signItem, driver);
-//      print("Tap ReadCard in menu");
-//      await driver.tap(signItem);
-//      print("Checking whether an element ActionButton is present");
-//      await isExist(cidButton, driver);
-//      print("Tab action button");
-//      await driver.tap(cidButton);
-//    });
-
-    tearDownAll(() async {
-      await Future.delayed(Duration(seconds: 3));
-      driver?.close();
-    });
   });
+
+
+
+  tearDownAll(() async {
+    await Future.delayed(Duration(seconds: 3));
+    driver?.close();
+  });
+
 }
