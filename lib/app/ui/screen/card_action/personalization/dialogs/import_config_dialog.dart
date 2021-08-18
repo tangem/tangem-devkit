ee77742ad6fe9b7c7aa275b258f33440dae27ad6
Anton Zhilenkov <azhilenkov@tangem.com>
2021-08-18 09:27:35 +0300

    fixed importing of personalization config

--- lib/app/ui/screen/card_action/personalization/dialogs/import_config_dialog.dart
+++ lib/app/ui/screen/card_action/personalization/dialogs/import_config_dialog.dart
@@ -3,6 +3,7 @@ import 'package:devkit/app/resources/app_resources.dart';
 import 'package:devkit/app/ui/widgets/app_widgets.dart';
 import 'package:devkit/commons/utils/app_attributes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ImportConfigDialog {
   final PersonalizationBloc _bloc;
@@ -37,34 +38,56 @@ class _ImportConfigWidgetState extends State<ImportConfigWidget> {
   final _controller = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           SizedBox(height: 5),
-          Container(
-            child: TextField(
-              key: ItemId.from(ItemName.personalizationImportInput),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                labelText: "Paste the configuration",
-                isDense: true,
-              ),
-              style: TextStyle(fontSize: AppDimen.itemTextSize),
-              onSubmitted: _onSubmit,
-              controller: _controller,
+          TextField(
+            key: ItemId.from(ItemName.personalizationImportInput),
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+              labelText: "Paste the configuration",
+              isDense: true,
             ),
+            minLines: 1,
+            maxLines: 25,
+            style: TextStyle(fontSize: AppDimen.itemTextSize),
+            onSubmitted: _onSubmit,
+            controller: _controller,
           ),
           SizedBox(height: 15),
-          ConstrainedBox(
-            constraints: const BoxConstraints(minWidth: double.infinity),
-            child: OutlinedButton(
-              key: ItemId.btnFrom(ItemName.personalizationImportInput),
-              child: TextWidget(Transl.of(context).menu_pers_import),
-              onPressed: _controller.text.isEmpty ? null : () => _onSubmit(_controller.text),
-            ),
-          )
+          Row(
+            children: [
+              OutlinedButton(
+                  onPressed: () async {
+                    final data = await Clipboard.getData(Clipboard.kTextPlain);
+                    final textData = data?.text ?? "";
+                    if (textData.isEmpty) return;
+
+                    _controller.value = TextEditingValue(text: textData);
+                  },
+                  child: Icon(Icons.paste)),
+              SizedBox(width: 8),
+              Expanded(
+                child: OutlinedButton(
+                  key: ItemId.btnFrom(ItemName.personalizationImportInput),
+                  child: TextWidget(Transl.of(context).menu_pers_import),
+                  onPressed: _controller.text.isEmpty ? null : () => _onSubmit(_controller.text),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
