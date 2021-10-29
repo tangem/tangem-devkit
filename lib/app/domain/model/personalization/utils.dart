bf46711e5ccb0797f8e4f4a6f82dba95945abcf8
Anton Zhilenkov <azhilenkov@tangem.com>
2021-10-29 19:05:52 +0300

    AND-1189 added fake S2C issuer

--- lib/app/domain/model/personalization/utils.dart
+++ lib/app/domain/model/personalization/utils.dart
@@ -123,6 +123,23 @@ class Utils {
     );
   }
 
+  static Issuer createFakeS2CIssuer() {
+    final name = "Start2Coin";
+    final dataPublic =
+        "045f16bd1d2eafe463e62a335a09e6b2bbcbd04452526885cb679fc4d27af1bd22f553c7deefb54fd3d4f361d14e6dc3f11b7d4ea183250a60720ebdf9e110cd26";
+    final dataPrivate = "11121314151617184771ED81F2BACF57479E4735EB1405083927372D40DA9E92";
+
+    final transPublic =
+        "0484c5192e9bfa6c528a344f442137a92b89ea835bfef1d04cb4362eb906b508c5889846cfea71ba6dc7b3120c2208df9c46127d3d85cb5cfbd1479e97133a39d8";
+    final transPrivate = "11121314151617184771ED81F2BACF57479E4735EB1405081918171615141312";
+    return Issuer(
+      name,
+      name + "\u0000",
+      KeyPairHex(dataPublic, dataPrivate),
+      KeyPairHex(transPublic, transPrivate),
+    );
+  }
+
   static Acquirer createDefaultAcquirer() {
     final name = "Smart Cash";
     final publicKey =
